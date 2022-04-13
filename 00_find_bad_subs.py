9e64a961cab480a5a38c8d7c34f78b19fab499bf
Simon Remote <ciranka@ARC-LIN-CPT07>
2022-04-13 11:49:47 +0200

    find bad subs is a standalone script 2

--- 00_find_bad_subs.py
+++ 00_find_bad_subs.py
@@ -21,35 +21,9 @@ from utils import get_behavioral_data, parse_overwrite
 
 # %%
 # Filepaths and settings
-
-sub = 1
 fpath_ds = FPATH_DS
 overwrite = False
 
-
-# %%
-# When not in an IPython session, get command line inputs
-# https://docs.python.org/3/library/sys.html#sys.ps1
-if not hasattr(sys, "ps1"):
-    defaults = dict(
-        sub=sub,
-        fpath_ds=fpath_ds,
-        overwrite=overwrite,
-    )
-
-    defaults = parse_overwrite(defaults)
-
-    sub = defaults["sub"]
-    fpath_ds = defaults["fpath_ds"]
-    overwrite = defaults["overwrite"]
-
-# Check inputs after potential overwriting
-if sub not in SUBJS:
-    raise ValueError(f"'{sub}' is not a valid subject ID.\nUse: {SUBJS}")
-if not fpath_ds.exists():
-    raise RuntimeError(PATH_NOT_FOUND_MSG.format(fpath_ds))
-if overwrite:
-    logger.info("`overwrite` is set to ``True``.")
 # %%
 
 exclude=list()# placeholder for excluding subjects
