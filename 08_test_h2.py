1f58055caacd4c3977ccb5d3aa5e673603335504
Simon Remote <ciranka@ARC-LIN-CPT07>
2022-04-27 18:47:34 +0200

    added time frequency

--- 08_test_h2.py
+++ 08_test_h2.py
@@ -18,6 +18,7 @@ import os
 import mne
 import numpy as np
 from mne.channels import find_ch_adjacency
+from mne.time_frequency import tfr_morlet
 
 from config import FPATH_DS, SUBJS, TRIGGER_CODES
 from utils import catch  # , parse_overwrite
@@ -26,13 +27,19 @@ from utils import catch  # , parse_overwrite
 # Path and settings
 fpath_ds = FPATH_DS
 overwrite = True
+# Settings for cluster test
+tfce = dict(start=0.1, step=0.05)
+# Time frequency
+n_cycles = 7
+alpha_freqs = np.arange(8, 12.5, 0.5)  # define frequencies of interest
+theta_freqs = np.arange(4, 7.5, 0.5)  # define frequencies of interest
+
 # Roi & toi
 ch_fronto_central = ["FCz", "FC1", "FC2", "FC3", "FC4", "Fz"]
 ch_posterior = ["Poz", "Po3", "Po4", "Oz", "O1", "O2", "Po7" "Po8"]
 toi_min = 0.3
 toi_max = 0.5
-# Settings for cluster
-tfce = dict(start=0.4, step=0.4)
+
 
 # List of all trigger combinations for a new image
 triggers_new_list = list(
@@ -86,7 +93,7 @@ epochs_complete = list(filter(None.__ne__, epochs))
 # needed for permutation test (n_observations (subs), time, n_vertices (channels)).
 
 # old images
-epochs_old_list = list(
+evokeds_old_list = list(
     [
         x[triggers_old]
         .crop(toi_min, toi_max)
@@ -97,9 +104,9 @@ epochs_old_list = list(
         for x in epochs_complete
     ]
 )
-epochs_old_arr = np.vstack(epochs_old_list)
+evokeds_old_arr = np.vstack(evokeds_old_list)
 # new images
-epochs_new_list = list(
+evokeds_new_list = list(
     [
         x[triggers_new]
         .crop(toi_min, toi_max)
@@ -110,22 +117,45 @@ epochs_new_list = list(
         for x in epochs_complete
     ]
 )
-epochs_new_arr = np.vstack(epochs_new_list)
-
+evokeds_new_arr = np.vstack(evokeds_new_list)
+# Concat data for use with cluster based permutation test
+X_h2a = [evokeds_old_arr, evokeds_new_arr]
 # %%
 # Calculate adjacency matrix between sensors from their locations
 adjacency, _ = find_ch_adjacency(
     epochs_complete[1].pick_channels(ch_fronto_central).info, "eeg"
 )
-
-# Extract data: transpose because the cluster test requires channels to be last
-# In this case, inference is done over items. In the same manner, we could
-# also conduct the test over, e.g., subjects.
-
-X = [epochs_old_arr, epochs_new_arr]
-
-# Calculate statistical thresholds
-t_obs, clusters, cluster_pv, h0 = mne.stats.spatio_temporal_cluster_test(
-    X, tfce, adjacency=adjacency, n_permutations=100
-)  # a more standard number would be 1000+
 # %%
+# Calculate statistical thresholds, confirms h2a
+t_obs, clusters, cluster_pv, h0 = mne.stats.spatio_temporal_cluster_test(
+    X_h2a, tfce, adjacency=adjacency, n_permutations=1000
+)
+# %%
+# plot the voltage, taking the average of all subjects
+# old images
+epochs_old_plot = list([x[triggers_old].average() for x in epochs_complete])
+# new images
+epochs_new_plot = list([x[triggers_new].average() for x in epochs_complete])
+# %%
+evokeds = dict(old=epochs_old_plot, new=epochs_new_plot)
+
+mne.viz.plot_compare_evokeds(evokeds, combine="mean", picks=ch_fronto_central)
+# %%
+# time frequency
+
+tfr_new_list = list(
+    [
+        tfr_morlet(
+            x[triggers_new].pick_channels(ch_fronto_central),
+            alpha_freqs,
+            n_cycles=n_cycles,
+            decim=7,
+            average=True,
+            return_itc=False,
+            n_jobs=1,
+        )
+        .crop(toi_min, toi_max)
+        .get_data()
+        for x in epochs_complete
+    ]
+)
