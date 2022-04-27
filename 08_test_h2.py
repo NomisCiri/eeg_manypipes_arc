b5f576e59b1c2bd5786932ac8cfd409751aaffee
Simon Remote <ciranka@ARC-LIN-CPT07>
2022-04-27 17:53:55 +0200

    permutation test over sensors

--- 08_test_h2.py
+++ 08_test_h2.py
@@ -16,6 +16,8 @@ import itertools
 import os
 
 import mne
+import numpy as np
+from mne.channels import find_ch_adjacency
 
 from config import FPATH_DS, SUBJS, TRIGGER_CODES
 from utils import catch  # , parse_overwrite
@@ -24,10 +26,14 @@ from utils import catch  # , parse_overwrite
 # Path and settings
 fpath_ds = FPATH_DS
 overwrite = True
+# Roi & toi
 ch_fronto_central = ["FCz", "FC1", "FC2", "FC3", "FC4", "Fz"]
 ch_posterior = ["Poz", "Po3", "Po4", "Oz", "O1", "O2", "Po7" "Po8"]
 toi_min = 0.3
 toi_max = 0.5
+# Settings for cluster
+tfce = dict(start=0.4, step=0.4)
+
 # List of all trigger combinations for a new image
 triggers_new_list = list(
     itertools.product(
@@ -75,19 +81,51 @@ epochs = [
 #  Keep only existing subs
 epochs_complete = list(filter(None.__ne__, epochs))
 # %%
-# Get a list of epochs in the desired timerange and with the desired channels
+# Get a list of epochs in the desired timerange and with the desired channels.
+# already put it into the format
+# needed for permutation test (n_observations (subs), time, n_vertices (channels)).
+
 # old images
-epochs_old = list(
+epochs_old_list = list(
     [
-        x[triggers_old].crop(toi_min, toi_max).pick_channels(ch_fronto_central)
+        x[triggers_old]
+        .crop(toi_min, toi_max)
+        .pick_channels(ch_fronto_central)
+        .average()
+        .get_data()
+        .transpose(1, 0)
         for x in epochs_complete
     ]
 )
+epochs_old_arr = np.vstack(epochs_old_list)
 # new images
-epochs_new = list(
+epochs_new_list = list(
     [
-        x[triggers_new].crop(toi_min, toi_max).pick_channels(ch_fronto_central)
+        x[triggers_new]
+        .crop(toi_min, toi_max)
+        .pick_channels(ch_fronto_central)
+        .average()
+        .get_data()
+        .transpose(1, 0)
         for x in epochs_complete
     ]
 )
+epochs_new_arr = np.vstack(epochs_new_list)
+
+# %%
+# Calculate adjacency matrix between sensors from their locations
+adjacency, _ = find_ch_adjacency(
+    epochs_complete[1].pick_channels(ch_fronto_central).info, "eeg"
+)
+
+# Extract data: transpose because the cluster test requires channels to be last
+# In this case, inference is done over items. In the same manner, we could
+# also conduct the test over, e.g., subjects.
+
+X = [epochs_old_arr, epochs_new_arr]
+
+# Calculate statistical thresholds
+t_obs, clusters, cluster_pv, h0 = mne.stats.spatio_temporal_cluster_test(
+    X, tfce, adjacency=adjacency, n_permutations=100
+)  # a more standard number would be 1000+
 # %%
